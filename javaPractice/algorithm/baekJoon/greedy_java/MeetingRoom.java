97d71f7628db938bb637ea1be6fa39e66991beb5
xi_jjun <rlawowns97@naver.com>
2021-08-10 12:20:29 +0900

    al 0810

--- javaPractice/algorithm/baekJoon/greedy_java/MeetingRoom.java
+++ javaPractice/algorithm/baekJoon/greedy_java/MeetingRoom.java
@@ -1,7 +1,10 @@
-// 아직 못풀었음. 
+// https://st-lab.tistory.com/145 의 해결 방법을 참고했음. 코드는 참고x
+// 결국 코드까지 참고... Arrays.sort에 대해 자세히 알아봐야할 것 같다. 
+// 익숙하지 않았고 완전 처음보는 것이었음.
 import java.io.*;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.StringTokenizer;
 
 class MeetingRoom{
@@ -9,11 +12,9 @@ class MeetingRoom{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        boolean[] times = new boolean[100000];
-        // ArrayList<Boolean> times = new ArrayList<>();
-        boolean check = true;
         int N = Integer.parseInt(br.readLine()); // meeting numbers
-        int[][] meetingTimes = new int[N][3];
+        int[][] meetingTimes = new int[N][2];
+
         
         for(int i = 0; i < N; i++) {
             String line = br.readLine();
@@ -24,55 +25,36 @@ class MeetingRoom{
 
             meetingTimes[i][0] = start;
             meetingTimes[i][1] = end;
-            meetingTimes[i][2] = end - start;
         }
 
-        // 1. start가 빠른 순으로 정렬
-        // 2. end-start 가 작은 순으로 정렬
-        Arrays.sort(meetingTimes, (a, b) -> a[0] - b[0]); // start 빠른 순 정렬
-        Arrays.sort(meetingTimes, (a, b) -> a[2] - b[2]); // end-start sorting
+        if(N == 1) {
+            System.out.println(1);
+            return;
+        }
 
-        boolean firstCheck = false, secondCheck = false;
+        // Arrays.sort(meetingTimes, (a, b) -> a[1] - b[1]); // end 빠른 순 정렬
+        // Arrays.sort(meetingTimes, (a, b) -> a[0] - b[0]);
+        Arrays.sort(meetingTimes, new Comparator<int[]>() { 
+            @Override 
+            public int compare(int[] o1, int[] o2) { 
+                if(o1[1] == o2[1]) {
+                    return o1[0] - o2[0];
+                }
+                return o1[1] - o2[1];
+            } });
+
+
+        int endTime = 0;
         int count = 0;
-
-        for(int[] meetingTime : meetingTimes) {
-            for(int i = meetingTime[0]; i <= meetingTime[1]; i++) {
-                if(times[i] == false || i == meetingTime[0] || i == meetingTime[1]) {
-                    if(i == meetingTime[0]) firstCheck = true;
-                    if(i == meetingTime[1]) secondCheck = true;
-                    times[i] = true;
-                }
-                else {
-                    check = false;
-                    for(int j = meetingTime[0]; j <= i; j++) {
-                        if(firstCheck == false && secondCheck == false)
-                            times[j] = false;
-                    }   
-                    break;
-                }
+        
+        for(int[] i : meetingTimes) {
+            if(i[0] >= endTime) {
+                count++;
+                endTime = i[1];
             }
-            if(check == true) count++;
-            check = true;
-            firstCheck = false;
-            secondCheck = false;
         }
 
         bw.write(count + "");
-
-
-
-
-
-        // bw.write("\n");
-
-        // for(int i = 0; i < N; i++) {
-        //     for(int j = 0; j < 3; j++) {
-        //         bw.write(meetingTimes[i][j] + "  ");
-        //     }
-        //     bw.write("\n");
-        // }
-
-
 
         bw.flush();
         bw.close();
