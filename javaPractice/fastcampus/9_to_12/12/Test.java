531df26672c25f14985636f02649d4d7d063f439
xi_jjun <rlawowns97@naver.com>
2021-08-12 00:12:37 +0900

    java2 0811

---
+++ javaPractice/fastcampus/9_to_12/12/Test.java
@@ -0,0 +1,6 @@
+class Test{
+    public static void main(String[] args) {
+        This test = new This(111, "324");
+        System.out.println("reference value : " + test);
+    }
+}
---
+++ javaPractice/fastcampus/9_to_12/12/This.java
@@ -0,0 +1,9 @@
+class This{
+    int aaa;
+    String bbb;
+    This(int aaa, String bbb) {
+        this.aaa = aaa;
+        this.bbb = bbb;
+        System.out.println("this : " + this);
+    }
+}
