760b93949ec124daf268d050dc45ca9e0224c25d
xi_jjun <rlawowns97@naver.com>
2021-08-20 23:57:17 +0900

    nomard coderVJS end

--- web_practice/nomadCoder/frontEnd/js/background.js
+++ web_practice/nomadCoder/frontEnd/js/background.js
@@ -1,9 +1,26 @@
-const images = ["0.jpeg", "1.jpeg", "2.jpeg"];
+const images = ["./img/0.jpeg", "./img/1.jpeg", "./img/2.jpeg", "./img/3.jpg"];
+const IMG_NUMBER = images.length;
+const bg = document.getElementById("bg1");
+const body = document.querySelector("body");
+console.log(bg);
+const chosenImage = images[Math.floor(Math.random() * images.length)]; // random number create
+// background-repeat : no-repeat;
+//     background-size : cover;
+const sytle1 = "background-repeat : no-repeat; background-size : cover;";
+const bgImage = "url('" + chosenImage + "')";
 
-const chosenImage = images[Math.floor(Math.random() * images.length)];
+body.style.backgroundImage = bgImage;
+// body.style.backgroundColor = "background-color: transparent;"
 
-const bgImage = document.createElement("img");
 
-bgImage.src = `img/${chosenImage}`;
 
-document.body.appendChild(bgImage);
+// const paintBgImage = (imgNumber) => {
+//     const bgImage = "url('" + images[imgNumber] + "')";
+//     body.style.backgroundImage = bgImage;
+// }
+
+// const bgImage = document.createElement("img");
+
+// bgImage.src = `img/${chosenImage}`;
+
+// document.body.appendChild(bgImage);
--- web_practice/nomadCoder/frontEnd/js/weather.js
+++ web_practice/nomadCoder/frontEnd/js/weather.js
@@ -1,5 +1,17 @@
+const API_KEY = "지웠다"; // 나중에 지워야 함.
+
 function onGeoOk(position) {
-    console.log(position);
+    const lat = position.coords.latitude;
+    const lon = position.coords.longitude;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+
+    fetch(url).then(response => response.json()).then(data => {
+        const weather = document.querySelector("#weather span:first-child");
+        const city = document.querySelector("#weather span:last-child");
+
+        weather.innerText = data.weather[0].main;
+        city.innerText = data.name;
+    }); // call url.then(json get).
 }
 function onGeoError() {
     alert("We can't get your location!");
