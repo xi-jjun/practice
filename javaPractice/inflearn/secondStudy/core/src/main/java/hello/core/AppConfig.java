a1fec50e6fc9f8b7f8fc2538168a3cea6e699e6c
xi_jjun <rlawowns97@naver.com>
2021-10-03 00:57:47 +0900

    SRP OCP DIP spring start

--- javaPractice/inflearn/secondStudy/core/src/main/java/hello/core/AppConfig.java
+++ javaPractice/inflearn/secondStudy/core/src/main/java/hello/core/AppConfig.java
@@ -9,22 +9,29 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
+    @Bean
     public MemberService memberService() {
         // 현재 나의 App에서는 MemberServiceImpl을 쓸거야!
         return new MemberServiceImpl(memoryRepository()); // MemberServiceImpl에 생성자 만들어서 넣어준다. Constructor Injection
     }
 
     // method와 return type으로 역할이 한눈에 보인다
-    private MemberRepository memoryRepository() {
+    @Bean
+    public MemberRepository memoryRepository() {
         return new MemoryMemberRepository(); // 현재 이걸로 쓸 것. 나중에 바뀌면 여기만 수정하면 된다.
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memoryRepository(), discountPolicy()); // Constructor Injection
     }
 
+    @Bean
     public DiscountPolicy discountPolicy() {
 //        return new FixDiscountPolicy();
         return new RateDiscountPolicy();
--- javaPractice/inflearn/secondStudy/core/src/main/java/hello/core/MemberApp.java
+++ javaPractice/inflearn/secondStudy/core/src/main/java/hello/core/MemberApp.java
@@ -3,12 +3,20 @@ package hello.core;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
-//        MemberService memberService = new MemberServiceImpl();
+//        MemberService memberService = new MemberServiceImpl(); // 구현에 의존하면 안된다. 제어는 AppConfig 가 하도록 했다.
+        // 아래 2줄은 OCP, DIP 지키기 위해 AppConfig라는 관리자를 만들기 위한 것.
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+
+        // 이제 순수 JAVA말고 Spring을 사용. Spring container에서 bean을 가져옴.
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
 
