1cf1540b54597e4c37bfc32015202ec737308882
xi_jjun <rlawowns97@naver.com>
2021-10-05 23:29:11 +0900

    springBean check

---
+++ javaPractice/inflearn/secondStudy/core/src/test/java/hello/core/beanFind/ApplicationContextInfoTest.java
@@ -0,0 +1,26 @@
+package hello.core.beanFind;
+
+import hello.core.AppConfig;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+class ApplicationContextInfoTest {
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    @Test
+    @DisplayName("Bean print")
+    void findAllBean() {
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            System.out.println(beanDefinitionName);
+        }
+    }
+    
+    @Test
+    void findBean() {
+        Object memberService = ac.getBean("memberService");
+        System.out.println("memberService = " + memberService);
+    }
+}
+
