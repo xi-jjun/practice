fd8864f3dd379b8b33ec055d1780fc88bce534e4
xi_jjun <rlawowns97@naver.com>
2021-10-30 01:56:15 +0900

    persistence Context

--- javaPractice/inflearn/jpa-basic/src/main/java/hellojpa/JpaMain.java
+++ javaPractice/inflearn/jpa-basic/src/main/java/hellojpa/JpaMain.java
@@ -41,17 +41,35 @@ public class JpaMain {
 //        }
 
         // UPDATE
+//        try {
+//            Member findMember = em.find(Member.class, 1L); // 찾으려는 Class , PK
+//            findMember.setName("Changed Name"); // Java 객체에서 값만 바꿔도 DB에 update 되는 GOD JPA
+//            /***
+//             * 원리
+//             * em.find(JPA 를 통해) 해서 가져오는 시점에 JPA가 해당 객체를 관리하게 된다.
+//             * JPA 가 변경이 됐는지 tx.commit 시점에 모두 확인을 한다.
+//             * 수정사항이 있으면 Update query 만들어서 날린다.
+//             */
+//
+//            tx.commit();
+//        } catch (Exception e) {
+//            tx.rollback();
+//        } finally {
+//            em.close(); // 반드시 종료해줘야만 한다.
+//        }
         try {
-            Member findMember = em.find(Member.class, 1L); // 찾으려는 Class , PK
-            findMember.setName("Changed Name"); // Java 객체에서 값만 바꿔도 DB에 update 되는 GOD JPA
-            /***
-             * 원리
-             * em.find(JPA 를 통해) 해서 가져오는 시점에 JPA가 해당 객체를 관리하게 된다.
-             * JPA 가 변경이 됐는지 tx.commit 시점에 모두 확인을 한다.
-             * 수정사항이 있으면 Update query 만들어서 날린다.
-             */
+            // new == 비영속 상태. member 라는 객체가 생성만 됐을 뿐 현재 JPA와는 아무 관련이 없다.
+            Member member = new Member();
+            member.setId(2L);
+            member.setName("UserB");
 
-            tx.commit();
+            // 영속 상태 - DB에 저장된 것 아님.
+            em.persist(member); // 이 코드일 때 Query 가 날라가지 않는다.
+
+//            em.detach(member); // member 를 다시 영속성 컨텍스트에서 지운다.
+//            em.remove(member); // 실제 DB에서 지운다.
+
+            tx.commit(); // 이 시점에 영속성에 있는 객체의 Query를 날린다.
         } catch (Exception e) {
             tx.rollback();
         } finally {
